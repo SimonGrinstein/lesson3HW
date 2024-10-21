d8a11673cfde6e503635be29f81aeae29201a1f7
SimonGrinstein <senja@web.de>
2024-10-21 14:39:33 +0200

    fixed script

--- script.js
+++ script.js
@@ -2,13 +2,20 @@
 
 let counter = 0;
 
+document.getElementById("counter").innerHTML= counter;
+
+
 const plus = document.getElementById("plus");
 const minus = document.getElementById("minus");
 
 plus.onclick = () => {
     counter++;
+    console.log("counter plus:" + counter);
+    document.getElementById("counter").innerHTML= counter;
 }
 
 minus.onclick = () => {
     counter--;
+    console.log("counter minus:" + counter);
+    document.getElementById("counter").innerHTML= counter;
 }
