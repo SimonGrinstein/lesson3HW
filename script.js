1c9d62958b771ec098f0445bd901e40379515a4b
SimonGrinstein <senja@web.de>
2024-10-21 13:16:53 +0200

    edit script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log(document.body);
+//console.log(document.body);
+
+let counter = 0;
+
+const plus = document.getElementById("plus");
+const minus = document.getElementById("minus");
+
+plus.onclick = () => {
+    counter++;
+}
+
+minus.onclick = () => {
+    counter--;
+}
